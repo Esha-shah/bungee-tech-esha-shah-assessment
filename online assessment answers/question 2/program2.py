7ec3f5ec42625f61c4fde9fee9108a733a063790
Esha-shah <68271678+Esha-shah@users.noreply.github.com>
2021-10-21 13:12:24 +0530

    Update program2.py

--- online assessment answers/question 2/program2.py
+++ online assessment answers/question 2/program2.py
@@ -2,3 +2,4 @@ import pandas as pd
 df = pd.read_csv("main2.csv")
 occupation_groups = df.groupby('occupation').agg({'age': [ 'min', 'max']})
 print(occupation_groups)
+occupation_groups.to_csv("Answer2.csv")
