683908d70c4804e805494df822401683e22c1efd
Esha-shah <68271678+Esha-shah@users.noreply.github.com>
2021-10-21 13:11:39 +0530

    Update program1.py

--- online assessment answers/question 1/program1.py
+++ online assessment answers/question 1/program1.py
@@ -4,3 +4,4 @@ decade_groups = df.groupby((df['Year']//10)*10)
 aggregate = {"Population": "max", "Violent": "sum", "Property": "sum","Murder":"sum","Forcible_Rape":"sum","Robbery":"sum","Aggravated_assault":"sum","Burglary":"sum","Larceny_Theft":"sum","Vehicle_Theft":"sum"}
 grouped_df = decade_groups.aggregate(aggregate)
 print(grouped_df)
+grouped_df.to_csv("Answer1.csv")
