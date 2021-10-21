67ade2de835e979f1f78162354fc38a441b1f3dc
Esha-shah <68271678+Esha-shah@users.noreply.github.com>
2021-10-21 13:12:54 +0530

    Update program3.py

--- online assessment answers/question 3/program3.py
+++ online assessment answers/question 3/program3.py
@@ -1,4 +1,7 @@
+import csv
 import pandas as pd
 df = pd.read_csv("main3.csv")
 sorted = df.sort_values(['Red Cards','Yellow Cards'],ascending=[False,False])
 print(sorted[['Team','Yellow Cards','Red Cards']])
+df=sorted[['Team','Yellow Cards','Red Cards']]
+df.to_csv("answer3.csv")
